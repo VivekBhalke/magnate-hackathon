b6ca87b50b0fb6d95bf0839a49ed1a1d07b29ba2
sojo06 <sajoshi06@gmail.com>
2025-03-05 09:39:46 +0530

    twiliio

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,26 @@
-// src/app.js
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
+
+
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
-const indexRouter = require('./routes/index');
-app.use('/api', indexRouter);
+
+
+app.get('/', (req, res) => {
+    
+});
 
 // Start server
 app.listen(PORT, () => {
