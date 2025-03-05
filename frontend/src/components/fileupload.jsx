188236e969accf650ae1c8fd9843fe7c7ddd6dfa
Vivek Bhalke <ajm.vivekbhalke@gmail.com>
2025-03-05 17:45:44 +0530

    added the apiroute

--- frontend/src/components/fileupload.jsx
+++ frontend/src/components/fileupload.jsx
@@ -1,13 +1,32 @@
 "use client";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { FileUpload } from "@/components/ui/file-upload";
+import { UserDataContext } from "../context/userContext";
 
 export function FileUploadDemo() {
   const [files, setFiles] = useState([]);
-  const handleFileUpload = (files) => {
-    setFiles(files);
-    console.log(files);
-  };
+  
+const handleFileUpload = async (files) => {
+  if (files && files[0] ) {  // Check if file and email are provided
+      const formData = new FormData();
+      formData.append("pdf", files[0]);          
+      const user = useContext(UserDataContext);
+      formData.append("email", user);           // Append email
+
+      try {
+          const response = await axios.post("http://localhost:5000/api/upload", formData, {
+              headers: {
+                  "Content-Type": "multipart/form-data",  // Set content type for form data
+              },
+          });
+          console.log("File uploaded successfully:", response.data);
+      } catch (error) {
+          console.error("Error uploading file:", error);
+      }
+  } else {
+      console.log("File or email missing!");
+  }
+};
 
   return (
     (<div
